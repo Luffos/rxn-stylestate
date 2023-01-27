8321cd20cdd0d85c60c73df6b2a09092bbaded04
Lucas <lgxm3z@gmail.com>
2023-01-27 10:04:27 -0300

    make code one line

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import { StyleSheet } from "react-native";
 
-const createStyle = <T>(style: () => T) => StyleSheet.create(style);
-
-export { createStyle };
+export const createStyle = <T>(style: () => T) => StyleSheet.create(style);
