ab131334ae548bcd3389cb816c7079d3b1ebcff1
Lucas <lgxm3z@gmail.com>
2023-01-27 09:38:04 -0300

    add basic files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { StyleSheet } from "react-native";
 
-const createStyle = <T>(style: () => T) => {
-  return StyleSheet.create(style);
-};
+const createStyle = <T>(style: () => T) => StyleSheet.create(style);
 
 export { createStyle };
